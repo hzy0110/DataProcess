fcf1feca6786beb772c9f70d38c604704b8e21ca
hzy0110 <hzy0110@hotmail.com>
2016-01-18 14:33:43 +0800

    Spark Streaming

--- spark/src/main/java/com/hzy/spark/WorkCount4.java
+++ spark/src/main/java/com/hzy/spark/WorkCount4.java
@@ -1,12 +1,53 @@
 package com.hzy.spark;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
 
 /**
  * Created by zy on 2016/1/16.
  */
 public class WorkCount4 {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(10));
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
 
-    SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+        // Split each line into words
+        JavaDStream<String> words = lines.flatMap(
+                new FlatMapFunction<String, String>() {
+                    public Iterable<String> call(String x) {
+                        return Arrays.asList(x.split(" "));
+                    }
+                });
 
+        // Count each word in each batch
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    public Tuple2<String, Integer> call(String s) {
+                        return new Tuple2<String, Integer>(s, 1);
+                    }
+                });
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey(
+                new Function2<Integer, Integer, Integer>() {
+                    public Integer call(Integer i1, Integer i2) {
+                        return i1 + i2;
+                    }
+                });
+
+        // Print the first ten elements of each RDD generated in this DStream to the console
+        wordCounts.print();
+
+        jssc.start();              // Start the computation
+        jssc.awaitTermination();   // Wait for the computation to terminate
+    }
 }
