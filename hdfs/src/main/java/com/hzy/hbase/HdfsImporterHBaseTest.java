cf719a7882db348e7bf0d2264706f7c9c721c88c
hzy0110 <hzy0110@hotmail.com>
2016-02-05 17:04:31 +0800

    HDFS2HBASE初步跑通

--- hdfs/src/main/java/com/hzy/hbase/HdfsImporterHBaseTest.java
+++ hdfs/src/main/java/com/hzy/hbase/HdfsImporterHBaseTest.java
@@ -6,7 +6,9 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -14,6 +16,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -23,7 +26,7 @@ import java.io.IOException;
 /**
  * Created by Hzy on 2016/2/4.
  */
-public class HdfsImporterHBaseTest  extends Configured implements Tool {
+public class HdfsImporterHBaseTest {
 
     /*static class HBaseTemperatureMapper<K> extends Mapper<LongWritable, Text, K, Put> {
         @Override
@@ -40,9 +43,10 @@ public class HdfsImporterHBaseTest  extends Configured implements Tool {
         }
     }*/
 
-    public class Hdfs2HBaseMapper extends Mapper<LongWritable, Text, Text, Text> {
+    public static class Hdfs2HBaseMapper extends Mapper<LongWritable, Text, Text, Text> {
         public void map(LongWritable key, Text line, Context context) throws IOException,InterruptedException {
             String lineStr = line.toString();
+            System.out.println("lineStr="+lineStr);
             int index = lineStr.indexOf(":");
             String rowkey = lineStr.substring(0, index);
             String left = lineStr.substring(index+1);
@@ -51,7 +55,7 @@ public class HdfsImporterHBaseTest  extends Configured implements Tool {
     }
 
 
-    public class Hdfs2HBaseReducer extends Reducer<Text, Text, ImmutableBytesWritable, Put> {
+    public static class Hdfs2HBaseReducer extends TableReducer<Text, Text, ImmutableBytesWritable> {
         public void reduce(Text rowkey, Iterable<Text> value, Context context) throws IOException,InterruptedException {
             String k = rowkey.toString();
             for(Text val : value) {
@@ -66,7 +70,7 @@ public class HdfsImporterHBaseTest  extends Configured implements Tool {
             }
         }
     }
-
+/*
     @Override
     public int run(String[] args) throws Exception {
         System.out.println("进入run");
@@ -93,7 +97,7 @@ public class HdfsImporterHBaseTest  extends Configured implements Tool {
         FileInputFormat.addInputPath(job, new Path(args[0]));
         job.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "observations");
         return job.waitForCompletion(true) ? 0 : 1;
-    }
+    }*/
 
 /*    public static void main(String[] args) throws Exception {
         int exitCode = ToolRunner.run(HBaseConfiguration.create(),
@@ -109,18 +113,31 @@ public class HdfsImporterHBaseTest  extends Configured implements Tool {
             System.exit(2);
         }
 
-        Job job = new Job(conf, "hdfs2hbase");
+        Job job = Job.getInstance(conf, "hdfs2hbase");
+
         job.setJarByClass(HdfsImporterHBaseTest.class);
+
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
         job.setMapperClass(Hdfs2HBaseMapper.class);
         job.setReducerClass(Hdfs2HBaseReducer.class);
 
         job.setOutputKeyClass(ImmutableBytesWritable.class);
         job.setOutputValueClass(Put.class);
 
-        job.setOutputFormatClass(TableOutputFormat.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
 
+        TableMapReduceUtil.initTableReducerJob("mytable", Hdfs2HBaseReducer.class, job);
+
+        //job.setOutputFormatClass(TableOutputFormat.class);
+/*
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
         job.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, otherArgs[1]);
+*/
+
+
 
         System.exit(job.waitForCompletion(true)?0:1);
     }
