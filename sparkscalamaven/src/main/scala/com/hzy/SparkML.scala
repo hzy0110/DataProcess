a758632bc511f44c8a87c62dffbbb37ed1651e66
hzy0110 <hzy0110@hotmail.com>
2016-02-25 23:43:58 +0800

    scala项目增加准备测试的代码

---
+++ sparkscalamaven/src/main/scala/com/hzy/SparkML.scala
@@ -0,0 +1,28 @@
+package com.hzy
+
+// General purpose library
+import scala.xml._
+
+// Spark data manipulation libraries
+import org.apache.spark.sql.catalyst.plans._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+// Spark machine learning libraries
+
+import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
+import org.apache.spark.ml.classification
+.LogisticRegression
+import org.apache.spark.mllib.evaluation
+.BinaryClassificationMetrics
+import org.apache.spark.ml.Pipeline
+/**
+ * Created by zy on 2016/2/23.
+ */
+object SparkML {
+  def main(args:Array[String]){
+
+
+  }
+}
