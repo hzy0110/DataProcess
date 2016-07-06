30740782c21f8d939feeea48957c442ae6971352
hzy0110 <hzy0110@hotmail.com>
2016-07-06 16:11:51 +0800

    增加配置文件读取

---
+++ mahout/src/main/java/com/hzy/self/PropertiesUtil.java
@@ -0,0 +1,48 @@
+package com.hzy.self;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.IRStatistics;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.GenericRecommenderIRStatsEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.apache.mahout.common.RandomUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Hzy on 2016/6/14.
+ */
+public class PropertiesUtil {
+    private static Properties properties = null;
+    public static void main(String[] args) throws IOException, TasteException {
+        properties = load();
+        System.out.println(getValue("channlelevel"));
+    }
+
+    public static Properties load()  throws IOException{
+        Properties properties = new Properties();
+        InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties");
+        properties.load(input);// 加载属性文件
+        input.close();
+        return properties;
+    }
+
+    public static String getValue(String name) {
+        return properties.getProperty(name);
+    }
+
+
+
+}
