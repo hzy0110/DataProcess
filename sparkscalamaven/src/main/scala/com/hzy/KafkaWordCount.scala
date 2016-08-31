c004379f62af389e8f0d715d632cf7981aab92ac
hzy0110 <hzy0110@hotmail.com>
2016-08-31 12:32:29 +0800

    成功读取kafka，并count输出

--- sparkscalamaven/src/main/scala/com/hzy/KafkaWordCount.scala
+++ sparkscalamaven/src/main/scala/com/hzy/KafkaWordCount.scala
@@ -18,17 +18,21 @@ object KafkaWordCount {
 
     val Array(zkQuorum, group, topics, numThreads) = args
     val sparkConf = new SparkConf().setAppName("KafkaWordCount")
-    val ssc =  new StreamingContext(sparkConf, Seconds(2))
+    val ssc =  new StreamingContext(sparkConf, Seconds(60))
     ssc.checkpoint("checkpoint")
 
     val topicMap = topics.split(",").map((_,numThreads.toInt)).toMap
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L))
-      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
-    wordCounts.print()
 
+    val words = lines.flatMap(_.split(" "))
+    //val wordCounts = words.map(x => (x, 1L)).reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(60), 2)
+    val wordCounts = words.map(word => (word, 1)).reduceByKey((a, b) => a + b)
+    //println(wordCounts.count())
+    //wordCounts.print()
+    wordCounts.saveAsTextFiles("word_count_results_"+System.currentTimeMillis)
+    println("---------------------------------start-------------------------------------------")
     ssc.start()
     ssc.awaitTermination()
+    println("---------------------------------awaitTermination-------------------------------------------")
   }
 }
