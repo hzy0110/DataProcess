167a85bef7e48077450c7ef7ef38e3a702dd37b4
Hzy <hzy0110@163.com>
2017-01-16 16:21:58 +0800

    在Yarn测试zhunian文件操作成功

--- sparkscalamaven/src/main/scala/com/hzy/hbase/SparkWordCount.scala
+++ sparkscalamaven/src/main/scala/com/hzy/hbase/SparkWordCount.scala
@@ -25,10 +25,12 @@ object SparkWordCount {
      */
     val conf = new SparkConf()
     conf.setAppName("SparkWordCount")
-    conf.setMaster("spark://ods18:7077")
-    //conf.setMaster("yarn-client")
+    //conf.setMaster("spark://ods18:7077")
+    conf.setMaster("yarn-client")
     //conf.setMaster("spark://192.168.3.185:17077")
     conf.set("spark.executor.memory","128M")
+    conf.set("spark.yarn.appMasterEnv.CLASSPATH",
+      "$CLASSPATH:/opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/*")
     conf.setJars(List("hdfs://ods18/zhunian/sparkscalamaven-1.0-SNAPSHOT.jar"))
 //    conf.set("spark.ui.port‌​","7077");
     //定义Spark的上下文
